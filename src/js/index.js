84959949ac8a055c95c57ee7e3bcb7e8d7e7d430
dezzogo <122938506+dezzogo@users.noreply.github.com>
2023-01-25 10:09:23 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const botaoTrailer = document.querySelector(".botao-trailer");
+const modal = document.querySelector(".modal");
+const botaoX = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const linkDoVideo = video.src;
+
+function alternarModal () {
+    modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkDoVideo);
+});
+
+botaoX.addEventListener("click", () => { 
+    alternarModal();
+    video.setAttribute("src", "");
+});
